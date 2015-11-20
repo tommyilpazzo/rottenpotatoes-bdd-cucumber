4f1e694e3fbd86d6ccf57ad08372548b48603e93
Tommaso <tommyilpazzo@gmail.com>
2015-11-20 09:07:24 +0000

    Fixed DEPRECATION WARNING

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,9 +10,9 @@ Rottenpotatoes::Application.configure do
 
   config.eager_load = true
 
-
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS
   config.assets.js_compressor = :uglifier
